e3e27926363ae5bfaab1b3dbf936ad34f1393202
Darren Harrison <dharrison2010@live.com>
2023-08-26 10:33:14 -0500

    Add Entity and Controller

---
+++ TimeTracker.API/Controllers/TimeEntryController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Shared.Entities;
+
+namespace TimeTracker.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TimeEntryController : ControllerBase
+{
+
+    private static readonly List<TimeEntry> _timeEntries = new()
+    {
+        new TimeEntry
+        {
+            Id = 1,
+            Project = "Time Tracker App",
+            End = DateTime.Now.AddHours(1)
+        }
+    };
+
+    [HttpGet]
+    public ActionResult<List<TimeEntry>> GetAllTimeEntries() => Ok(_timeEntries);
+}
---
+++ TimeTracker.Shared/Entities/TimeEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeTracker.Shared.Entities;
+
+public class TimeEntry
+{
+    public int Id { get; set; }
+    public required string Project { get; set; }
+    public DateTime Start { get; set; } = DateTime.Now;
+    public DateTime? End { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public DateTime? DateUpdated { get; set; }
+}
+
