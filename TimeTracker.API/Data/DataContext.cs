c592dfe661e120f4a87d0d24c7333cf91e5ed245
Darren Harrison <dharrison2010@live.com>
2023-08-28 09:18:01 -0500

    Add DbContext

---
+++ TimeTracker.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeTracker.Shared.Entities;
+
+namespace TimeTracker.API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    public DbSet<TimeEntry> TimeEntries { get; set; }
+}
+
